8a8c350355bec15026eb932db6cd7fb39a5f15e9
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2012-09-01 23:11:19 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Porka::Application.config.secret_token = '3df0b660c585535421f83766cae460abf079ea5b6e392042ee4d84d898dc5a8190e76014afc5d9238d12baeba565e2df9ffc78474dbb05a3e88c58808a2062bf'
