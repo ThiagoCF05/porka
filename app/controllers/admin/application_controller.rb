44ab8670cd34a8fa1392d5d3dc38630245c391fc
Daniel Morais <damorais@gmail.com>
2012-11-12 23:48:31 -0200

    ...

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -3,19 +3,19 @@ class Admin::ApplicationController < ActionController::Base
 
   def current_administrator
 
-    if session[:current_administrator_id] != nil
-      @_current_administrator = Administrador.find(session[:current_administrator_id])
-    else
-      @_current_administrator = nil
-    end
+      #if session[:current_administrator_id] != nil
+        #@_current_administrator = Administrador.find(session[:#current_administrator_id])
+        #else
+        #@_current_administrator = nil
+        #end
 
-    return @_current_administrator
+      #return @_current_administrator
   end
 
   def administrador_logado
-    if self.current_administrator == nil
-      flash[:notice] = 'Área Restrita: necessário login'
-      redirect_to :controller => 'Logins', :action => 'index'
-    end
+      #    if self.current_administrator == nil
+      #flash[:notice] = 'Área Restrita: necessário login'
+      #redirect_to :controller => 'Logins', :action => 'index'
+      #end
   end
 end
