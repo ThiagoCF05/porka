6a5ec71f3ef2b9cfca6df7a67678c58900c82f68
Daniel Morais <damorais@gmail.com>
2012-11-14 01:04:36 -0200

    alterações no header, acréscimo da página de produtos

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,30 +2,31 @@ class MainController < ApplicationController
 
   # GET /index
   # GET /index.json
-  def index
-    @ofertas = self.getOfertas
-    @destaque = self.getDestaque
-    @cooperativas = self.getCooperativas
-
-    respond_to do |format|
-      format.html # index.html.erb
-      #format.json { header : ok }
+    def index
+        @ofertas = self.getOfertas
+        #@destaque = self.getDestaque
+        @oferta = self.getOfertaDestaque  
+        @cooperativas = self.getCooperativas
+        
+        respond_to do |format|
+            format.html # index.html.erb
+            #format.json { header : ok }
+        end
     end
-  end
-
-  def getCooperativas
-    @cooperativas = Cooperativa.where(:status_id => Status.find_by_descricao('Ativo')).shuffle
-    @cooperativas = @cooperativas[0..5]
-    return @cooperativas
-  end
-
-  def getOfertas
-    @ofertas = Oferta.order("created_at desc").limit(10)
-    return @ofertas
-  end
-
-  def getDestaque
-    @destaque = Destaque.order("created_at desc").limit(1)[0]
-    return @destaque
-  end
+    
+    def getCooperativas
+        @cooperativas = Cooperativa.where(:status_id => Status.find_by_descricao('Ativo')).shuffle
+        @cooperativas = @cooperativas[0..5]
+        return @cooperativas
+    end
+    
+    def getOfertas
+        @ofertas = Oferta.order("created_at desc").limit(10)
+        return @ofertas
+    end
+    
+    def getOfertaDestaque
+        @oferta = Destaque.order("created_at desc").limit(1)[0].oferta
+        return @oferta
+    end
 end
